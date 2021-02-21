a744f6a72aeed7d96287e4e0509a0a7033642c10
LiliiaGab <liliia041988@gmail.com>
2021-02-21 15:35:51 +1300

    Axios functions has been checked

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,15 @@
 import React from "react";
+import axios from "axios";
 
 export default function Form() {
+      function handleResponse(response) {
+        alert (`The weather in Auckland is ${response.data.main.temp}°C`)
+    }
+        let apiKey = "ac2523706a3a3cb29b4282c1c91e736e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="col-6">
       <form id="seacrh-form">
