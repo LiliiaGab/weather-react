1fde5a5797bf6b8fee520332cd5f59cf5c9af661
LiliiaGab <liliia041988@gmail.com>
2021-02-25 15:48:20 +1300

    form section is updating

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useState }  from "react";
 import axios from "axios";
 
 export default function Form() {
+const [place, setPlace] = useState(false);
       function handleResponse(response) {
-      console.log(response);
+        console.log(response)
+        setPlace(true);
     }
-        let apiKey = "ac2523706a3a3cb29b4282c1c91e736e";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
-  return (
+     
+  if (place){
+return (
     <div className="findcity">
       <form id="seacrh-form">
         <div classname="row">
@@ -33,4 +32,15 @@ export default function Form() {
       </form>
       </div>
   );
+  } else {
+      const apiKey = "ac2523706a3a3cb29b4282c1c91e736e";
+      let city = "Auckland";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
 }
+}
+  
+
+  
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
 const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
 function handleResponse(response) {
@@ -41,8 +41,7 @@ if (ready) {
   );
 } else {
       const apiKey = "ac2523706a3a3cb29b4282c1c91e736e";
-        let city = "Auckland";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
